a9ce4c5cc6ff365b87db000ef56f7a1f25367ff2
Taida <taida.carmona@xeria.es>
2026-04-07 23:20:18 +0200

    Añado site a astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://taidacr.github.io', 
+  site: 'https://taidacr.github.io/consultoria-web', 
   
   base: '/consultoria-web',
 });
