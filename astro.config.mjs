4b365356e61322f96d93841416c004e97736d00f
Taida <taida.carmona@xeria.es>
2026-04-20 19:56:55 +0200

    añado política de privacidad y aviso legal

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
-const isVercel = process.env.VERCEL === 'true';
 
 // https://astro.build/config
 export default defineConfig({
   site:'https://consultoriasalqualis.cl',
   base: '/',
-
-  //   site: 'https://taidacr.github.io/',
-  // base: '/',
   integrations: [sitemap()],
 });
