deaee962b3f0cb02daf2f66bde72845ce03b5bf6
Taida <taida.carmona@xeria.es>
2026-04-21 21:30:22 +0200

    Retoques textos legales, centros acreditados a js

---
+++ src/data/accreditations.js
@@ -0,0 +1,71 @@
+export const paisesAcred = [
+  {
+    id: "spain",
+    country: "ESPAÑA",
+    centers: [
+      "Hospital Universitario Gregorio Marañón. Hospital Materno-infantil. SERMAS. Madrid",
+      "Hospital Universitario de Cruces. Baracaldo. Osakidetza. Vizcaya.",
+      "Hospital del Alto Deba en Mondragón, Osakidetza. Guipuzcoa.",
+      "Hospital Universitario Costa del Sol. Marbella. SAS. Málaga.",
+      "Clínica Universitaria Fundación Jiménez Diaz. Grupo Quirón. Madrid",
+      "Hospital Universitario Quirón Pozuelo de Alarcón en Madrid",
+      "Hospital Clínica Dexeus Quirón en Barcelona",
+      "Hospital Quirón Córdoba",
+      "Hospital Teknon Quirón. Barcelona",
+      "Hospital Quirón Barcelona",
+      "Hospital Quirón Torrevieja. Alicante",
+      "Hospital Universitario Quirón La Luz. Madrid",
+      "Hospital La Zarzuela. BUPA-SANITAS. Madrid",
+      "Hospital La Moraleja. BUPA-SANITAS. Madrid",
+      "Hospital CIMA. BUPA-SANITAS Barcelona",
+      "Hospital Nuestra Señora del Mar. BUPA-SANITAS Madrid",
+      "Hospital Vithas XANIT Internacional Benalmádena. Málaga",
+      "Hospital Vithas Almeria",
+      "Hospital Vithas Granada",
+      "Hospital Vithas Nuestra Señora de América. Madrid",
+      "Hospital Zorrotzaurre en Bilbao. Vizcaya.",
+      "Hospital Clínica Benidorm. Alicante",
+      "Clínica Universitaria de Navarra. Pamplona",
+      "Clínica Universitaria de Navarra. Madrid",
+      "Clínica Servidigest. Barcelona",
+      "Clinica Emby. Clínica de reproducción asistida. Oviedo. Asturias",
+      "Centro de salud. Oñati. Osakidetza. Guipúzcoa",
+      "Centro de salud el Llano. SESPA. Asturias",
+      "Servicio de atención a domicilio Hospital Gregorio Marañón. SERMAS. Madrid",
+      "Grupo Quirón: Acreditación Enterprises",
+      "Grupo Vithas: Acreditación Enterprises"
+    ]
+  },
+  {
+    id: "peru",
+    country: "PERÚ",
+    centers: [
+      "Hospital Alberto Barton. Grupo Ibt. El Callao.",
+      "Hospital Guillermo Kaelin. Villa María del Triunfo. Grupo Ibt. Lima",
+      "Clínica San Pablo Surco. Grupo San Pablo. Lima",
+      "Clínica Jesús del Norte. Grupo San Pablo. Lima",
+      "Clínica San Gabriel. Grupo San Pablo. Lima",
+      "Clínica Ricardo Palma. Lima",
+      "Policlínico Alberto Barton. Grupo Ibt. El Callao.",
+      "Policlínico Guillermo Kaelin. Villa María del Triunfo. Grupo Ibt. Lima",
+      "Servicios de atención a domicilio de Grupo Ibt"
+    ]
+  },
+  {
+    id: "colombia",
+    country: "COLOMBIA",
+    centers: [
+      `Grupo Colsanitas, Keralty Bogotá con tres centros: 
+      - Clínica Reina Sofía
+      - Clínica Pediátrica Colsanitas
+      - Centro ambulatorio Colsanitas`
+    ]
+  },
+  {
+    id: "chile",
+    country: "CHILE",
+    centers: [
+      "Servicio de atención a domicilio Hospital Padre Hurtado. Servicio de Salud Sur Oriente. Santiago de Chile"
+    ]
+  }
+];
