50f483fc2861c006bb9f51554753467fbd7d1a57
Taida <taida.carmona@xeria.es>
2026-04-22 20:13:09 +0200

    team.js, ajustes SEO, optimización

---
+++ src/data/team.js
@@ -0,0 +1,253 @@
+export const team = [
+    {
+        name: "Jose Antonio García Ruiz",
+        role: "Director Consultoría",
+        image: "/assets/team/JGarcia.webp",
+        icon: "/assets/icon/stethoscope.svg",
+        description: "Socio fundador y Director de Consultoría Saquialis. Médico, surveyor de JCI desde hace 25 años. Ha realizado labores de consultoría en mas de 100 establecimientos de salud.",
+        cvdestacado: [
+            "Licenciado en Medicina. Universidad de Granada",
+            "Consultor y Surveyor Senior, Joint Commission International FAD",
+            "Experto en Calidad y Seguridad Clínica. Escuela Andaluza Salud Pública",
+            "Experto Universitario en Gestión Sanitaria. Universidad de Granada. EASP.",
+            "Diplomado en Alta Dirección de Empresas AD1. Instituto Internacional San Telmo",
+            "Diplomado en Gestión Hospitalaria. EADA"
+        ],
+
+        cvconsultoria: [
+            "Socio Director Consultoría Salqualis",
+            "Múltiples proyectos como consultor de Calidad en diferentes centros sanitarios para acreditación J.C.I.",
+            "Asistencias técnicas específicas para diferentes centros, destacando Grupo Quirón, Sanitas / BUPA y Vithas. A nivel internacional con consultorías en Servicio de Salud en Chile, y centros de Perú y Colombia.",
+            "Ha participado en auditoría y consultoría de JCI en más de 100 hospitales y otros centros sanitarios."
+        ],
+
+        cvtrayectoria: [
+            "Director Gerente del Hospital Costa del Sol. Marbella",
+            "Director de Sistemas de Información y Calidad del Hospital Costa del Sol. Marbella",
+            "Director Provincial de Atención Sanitaria. Málaga",
+            "Cooperación internacional en Angola, El Salvador y Honduras en diversos periodos",
+            "Profesor asociado a la Escuela Andaluza de Salud Pública. Profesor colaborador en las áreas de Calidad, Seguridad Clínica, Gestión y colaboración internacional.",
+            "Profesor en el Máster de Gestión Hospitalaria. Universidad de Alcalá de Henares",
+            "Profesor en el Máster de Ingeniería hospitalaria. Universidad de Málaga"
+        ]
+    },
+    {
+        name: "Salvador Rodríguez Pérez",
+        role: "Gestión de Infraestructuras y Tecnología",
+        image: "/assets/team/SRodriguez.webp",
+        icon: "/assets/icon/helmet.svg",
+        description: "Ingeniero Superior Industrial. Especializado en Ingeniería Hospitalaria y Alta Dirección. Experto en seguridad de Instalaciones, gestión de tecnologías sanitarias y puesta en marcha de hospitales de alta complejidad.",
+        cvdestacado: [
+            "Ingeniero Superior Industrial del ICAI (ESPECIALIDAD MECÁNICA) Universidad Pontificia Comillas. Madrid.",
+            "Máster en Ingeniería y Gestión de Infraestructuras Hospitalarias Universidad de Málaga.",
+            "Programa de alta dirección (ad-1). Instituto Internacional San Telmo.",
+            "Auditor de sistemas de la calidad Aenor. Ministerio de Ciencia y Tecnología y Ministerio de Defensa.",
+            "Técnico superior de prevención de riesgos laborales en las especialidades de seguridad e higiene industrial. Instituto de Post-Grado de ICADE."
+        ],
+
+        cvconsultoria: [
+            "Asistencia y consultoría en más de 30 centros y hospitales.",
+            "Referente en Gestión y seguridad de las instalaciones.",
+            "Referente en apoyo al programa de prevención y control de la infección.",
+            "Referente en gestión de tecnologías sanitarias."
+        ],
+
+        cvtrayectoria: [
+            "INGHO FACILITIES & MANAGEMENT S.L. Director Gerente - Ingeniero consultor.",
+            "VINCLES SOLUCIONES SECTOR DE LA SALUD, SL. Gerente del Área de Facility Management y director de INGHO Facility Management, SL.",
+            "EMPRESA PÚBLICA HOSPITAL COSTA DEL SOL. Director de Servicios Generales e Ingeniería.",
+            "Coordinador de actuaciones tendentes a la Puesta en Marcha del Hospital Costa Del Sol en Marbella.",
+            "INGENIERÍA Y ARQUITECTURA G7 ESTUDIOS Y PROYECTOS. Ingeniero Consultor y Socio."
+        ]
+    },
+    {
+        name: "Carmen Cortes Martínez",
+        role: "Seguridad Clínica y Procesos médicos",
+        image: "/assets/team/CCortes.webp",
+        icon: "/assets/icon/stethoscope.svg",
+        description: "Doctora en Medicina. Médico Especialista en Medina Preventiva y Salud Público. Larga trayectoria en gestión de centros sanitarios. Socia fundadora en Consultoría Salqualis.",
+        cvdestacado: [
+            "Licenciada en Medicina. Universidad de Granada (UGR)",
+            "Médico Especialista en Medicina Preventiva y Salud Pública.",
+            "Doctor en Medicina. UGR",
+            "Máster Universitario en Salud Pública y Administración Sanitaria. Escuela Andaluza de Salud Pública. UGR",
+            "Experto Universitario en Epidemiología e Investigación Clínica. Escuela Andaluza de Salud Pública. UGR",
+            "Máster en Especialización en Gestión y Metodología de la Calidad Asistencial. Universidad Autónoma de Barcelona."
+        ],
+
+        cvconsultoria: [
+            "Socio Consultoría Salqualis.",
+            "Asistencia y consultoría en grupos como SANITAS/BUPA, Quirón y Servicios de Salud de Chile.",
+            "Referente en trazadores clínicos, prevención control de la infección, mejora de la calidad, evaluación, atención centrada en el paciente y liderazgo.",
+            "Referente en Prevención control de la infección, mejora de la calidad, evaluación, atención centrada en el paciente y liderazgo.",
+            "Referente para programa de trasplantes en acreditación.",
+            "Referente en consultoría de acreditación en centros académicos e investigadores."
+        ],
+
+        cvtrayectoria: [
+            "Dir. General de Calidad, Investigación y Gestión del conocimiento. Sevilla. Consejería de Salud. J.A.",
+            "Dir. General de Asistencia Sanitaria: SAS. Sevilla. Consejería de Salud. J.A.",
+            "Cooperación internacional en Angola, El Salvador y República Dominicana en diversos periodos.",
+            "Dir. Médico Hospital Vithas Málaga.",
+            "Dir. Gerente de los hospitales: Universitario Regional de Málaga y Hospital Universitario de Málaga. SAS",
+            "Dir. Gerente Hospital Comarcal de la Axarquía. Vélez Málaga.",
+            "Dir. Médica. Hospital Comarcal de Antequera.",
+            "Dir. asistencial - Distrito sanitario de atención primaria Costa del Sol.",
+            "Médico en epidemiología y salud pública distrito Málaga. SAS"
+        ]
+    },
+    {
+    name: "Victoria Uribe Pérez",
+    role: "Seguridad Clínica y Procesos médicos",
+    image: "/assets/team/VUribe.webp",
+        icon: "/assets/icon/stethoscope.svg",
+    description: "Médico Especialista en Medicina Interna. Referente en calidad y seguridad de servicios médicos. Consultora experta en trazadores clínicos en Consultoría Salqualis.",
+    cvdestacado: [
+        "Licenciada en Medicina. Universidad de Málaga.",
+        "Médico Especialista en Medicina Interna.",
+        "Máster Universitario en Ecografía Clínica. Universidad de Huelva.",
+        "Máster Universitario en Enfermedades Autoinmunes. Universidad de Huelva.",
+        "Experto Universitario en Resistencias Antimicrobianas y Optimización del uso de antimicrobianos. Universidad de Sevilla.",
+        "Experto Universitario en Infecciones por el VIH y Enfermedades asociadas. Universidad de Sevilla."
+    ],
+
+    cvconsultoria: [
+        "Asistencia y consultoría de JCI en hospitales en España y Chile.",
+        "Referente en trazadores clínicos, evaluación y asistencia a pacientes.",
+        "Referente en Metas de Seguridad de pacientes.",
+        "Referente en gestión de sistemas de información clínica asistencial e implantación de registros clínicos.",
+        "Referente en gestión y uso de la medicación."
+    ],
+
+    cvtrayectoria: [
+        "Médico internista en Hospital Costa del Sol de Marbella.",
+        "Médico internista en Hospital Quirón.",
+        "Médico internista en Hospital Vithas."
+    ]
+},
+{
+    name: "Juan Ignacio Barrachina",
+    role: "Seguridad Clínica y Procesos médicos",
+    image: "/assets/team/barranchina.webp",
+        icon: "/assets/icon/stethoscope.svg",
+    description: "Médico experto en Gestión Hospitalaria (ESADE) con una trayectoria de alto nivel como CEO y Gerente de consultoría en PwC e Iasist. Especialista en planificación estratégica y reorganización de procesos asistenciales, ha liderado proyectos de acreditación internacional y eficiencia operativa en más de 50 centros.",
+    cvdestacado: [
+        "Licenciado en Medicina. Universidad de Barcelona.",
+        "Diplomado en Gestión Hospitalaria. Escuela Superior de Administración y Dirección de Empresas (ESADE)."
+    ],
+
+    cvconsultoria: [
+        "Asistencia y consultoría en más de 50 centros para consultoría de calidad y de gestión.",
+        "Elaboración del Plan Estratégico de 20 empresas del ámbito de salud.",
+        "Desde el año 2011 preparación de diversas organizaciones para la acreditación de la JCI en Europa y América Latina.",
+        "Reorganización de procesos asistenciales y de gestión en cinco unidades asistenciales de grandes hospitales.",
+        "Establecimiento de líneas de mejora en la gestión de los contratos con los financiadores.",
+        "Implantación de un sistema de información clínico basado en GRD en el Instituto Mexicano del Seguro Social (IMSS).",
+        "Definición de un modelo de evaluación de los centros proveedores de asistencia socio sanitaria y psiquiátrica.",
+        "Desarrollo de un modelo de gestión de proyectos en una agencia europea (Unión Europea).",
+        "Evaluación de los resultados asistenciales de la cirugía oncológica en una Región de España.",
+        "Análisis de la casuística hospitalaria en más de cuarenta hospitales de España, Portugal y América Latina."
+    ],
+
+    cvtrayectoria: [
+        "Consultor Sénior en Consultora de Hospitales SA (CODEH).",
+        "Adjunto al director general en Centro Inmunológico de Cataluña.",
+        "Gerente de Consultoría en PwC (PricewaterhouseCoopers).",
+        "Director de Proyectos en Iasist SA.",
+        "Gerente (CEO) de la Provincia de Barcelona de las Hermanas Hospitalarias."
+    ]
+},
+{
+    name: "Diego García Cortés",
+    role: "Gestión de la Medicación",
+    image: "/assets/team/DGarcia.webp",
+        icon: "/assets/icon/pill.svg",
+    description: "Farmacéutico Especialista en Farmacia Hospitalaria, con una destacada trayectoria en NHS de Reino Unido. Experto en farmacia clínica, especializado en el despliegue del modelo JCI para gestión del medicamento.",
+    cvdestacado: [
+        "Grado de Farmacia, Universidad de Sevilla.",
+        "Farmacéutico especialista en Farmacia Hospitalaria Clínica.",
+        "Pharmacist independent prescriber. University of Hallam. U.K.",
+        "Postgrado de Farmacia Clínica. University of Bradford. U.K."
+    ],
+
+    cvconsultoria: [
+        "Asistencia y consultoría en centros para despliegue del modelo de gestión y uso de la medicación de JCI.",
+        "Referente en Metas de seguridad de pacientes especialmente relativas a medicación de alto riesgo.",
+        "Referente en Calidad y Seguridad.",
+        "Referente en revisión de procesos asistenciales."
+    ],
+
+    cvtrayectoria: [
+        "FEA Farmacia Hospitalaria Servicio Andaluz de Salud 2024-presente",
+        "Jefe de Servicio Farmacia Hospitalaria Hospital Quiron Marbella 2023",
+        "Farmacéutico Hospital Costa del Sol. Marbella. SAS.",
+        "Farmacéutico Hospital Universitario Málaga. SAS.",
+        "Farmacéutico Hospital Regional de Málaga. SAS.",
+        "Farmacéutico de Hospital Quirón Marbella.",
+        "Band 8ª Senior clinical pharmacist. Jefe de sección. Chesterfield Royal Hospital. NHS. U.K.",
+        "Band 7ª Senior clinical pharmacist. Chesterfield Royal Hospital. NHS. U.K.",
+        "Band 7/6ª Senior clinical pharmacist. Doncaster & Bassetlaw Teaching Hospitals. Doncaster. NHS. U.K.",
+        "Clinical research associate. Unidad de investigación y ensayos clínicos. IBIMA. Málaga.",
+        "Senior clinical pharmacist. BMI hospitals. Sheffield. U.K."
+    ]
+},
+{
+    name: "José Carlos Martín Santos",
+    role: "Cuidados de Enfermería",
+    image: "/assets/team/JCMartin.webp",
+        icon: "/assets/icon/mask.svg",
+    description: "Amplia experiencia en Dirección de Enfermería y Calidad en centros hospitalarios. Consultor en implantación y desarrollo de competencias técnicas de enfermería y evaluación bajo el modelo JCI.",
+    cvdestacado: [
+        "Grado de enfermería en la Universidad de Salamanca.",
+        "Programa de desarrollo en liderazgo para la gestión de enfermería por la Universidad de Nebrija.",
+        "Auditor de sistemas de la calidad.",
+        "Master en gestión y liderazgo Universidad Europea."
+    ],
+
+    cvconsultoria: [
+        "Asistencia y consultoría en hospitales en España.",
+        "Referente en Evaluación y asistencia a pacientes, modelos de atención de enfermería, planes de cuidados.",
+        "Referente en Metas de Seguridad de pacientes.",
+        "Referente en Calidad y Seguridad.",
+        "Referente en cualificación profesional competencias técnicas de enfermería y otros profesionales de salud.",
+        "Referente en Sistemas de evaluación de enfermería e indicadores."
+    ],
+
+    cvtrayectoria: [
+        "Actualmente Director Enfermería en Hospital Hospiten en Estepona.",
+        "Director Enfermería en Hospital Vithas Málaga.",
+        "Supervisor de enfermería en el hospital Quirón Teknon acreditado JCI.",
+        "Supervisor de Calidad en Hospital Vithas Xanit Internacional acreditado JCI.",
+        "Jefe de Calidad y enfermero referente en infecciones en Hospital Vithas Málaga.",
+        "Actividad de enfermería en el Hospital Vall D’Hebron de Barcelona.",
+        "Investigador colaborador en instituto de investigación Vall d`Hebron. Barcelona."
+    ]
+},
+{
+    name: "Victoria Reina Fernández",
+    role: "Documentación clínica",
+    image: "/assets/team/VReina.webp",
+        icon: "/assets/icon/doc.svg",
+    description: "Técnico experto en documentación clínica y codificación. Conocimiento avanzado en metodología de calidad. Experiencia como responsable de Calidad y Documentación Clínica en hospitales.",
+    cvdestacado: [
+        "Técnico Superior en Documentación Sanitaria.",
+        "Curso de Estadística Avanzada, Madrid.",
+        "Formación en metodología Lean Healthcare, Madrid.",
+        "Curso Auditor Interno Norma ISO/UNE.",
+        "Curso sobre Fundamentos de la Transición a la Norma ISO 9001."
+    ],
+
+    cvconsultoria: [
+        "Experta en Codificación Clínica CIE-10.",
+        "Referente en metodología de calidad y seguridad del paciente.",
+        "Asesoría practica en metodología para la gestión de procesos y mejora continúa basada en riesgo.",
+        "Elaboración cuadro de mando de calidad y seguridad. Monitorización de indicadores asistenciales."
+    ],
+
+    cvtrayectoria: [
+        "Jefa del Departamento de Calidad, Seguridad del Paciente y Documentación Clínica. Vithas Málaga junio 2020 – actualidad.",
+        "Codificación clínica en Departamento de Calidad en Hospital Vithas Xanit Internacional septiembre 2009 – junio 2020.",
+        "Codificación Clínica en Hospital Quirón Málaga enero-septiembre 2009."
+    ]
+}
+]
